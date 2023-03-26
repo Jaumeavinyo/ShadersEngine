19fa637858821e22cea259bd78f3e31deb2fdaf6
jas3 <36154250+Jaumeavinyo@users.noreply.github.com>
2023-03-26 20:17:38 +0200

    solved major bug, triangle showing with shaders

--- Engine/Code/engine.cpp
+++ Engine/Code/engine.cpp
@@ -302,7 +302,7 @@ void Init(App* app)
         "layout(location = 0) out vec4 color;\n"
         "void main()\n"
         "{\n"
-        "color = vec4(1.0,0.5,0.0,1.0);\n"
+        "color = vec4(1.0,0.0,0.0,1.0);\n"
         "}\n"
         ;
     
